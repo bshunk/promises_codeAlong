6603d31a7743009db4eb69d525da4c027b585962
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-05 09:34:28 -0500

    work on code along with joe

--- Javascripts/events.js
+++ Javascripts/events.js
@@ -1,52 +1,17 @@
-"use strict";
+// "use strict";
 
+// var activateEvents = function() {
+// 	cardWrapper.addEventListener("click", cardStyle.highlightCard);
+// 	input.addEventListener("keyup", function() {
+// 		console.log("Just checking");
+// 		if (event.keyCode === 13) {
+// 			console.log
+// 		}
+// 	})
+// }
 
+// module.exports = activateEvents;
 
-
-
-
-
-
-
-var activateEvents = function() {
-	cardWrapper.addEventListener("click", cardStyle.highlightCard);
-	input.addEventListener("keyup", function() {
-		console.log("Just checking");
-		if (event.keyCode === 13) {
-			console.log
-		}
-	})
-}
-
-
-
-
-
-
-
-
-
-
-module.exports = activateEvents;
-
-
-
-// NODE terminology
+// NODE terminology- require the file runs the files and returns whatever the file returns 
 // require('./carLotPromises')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
