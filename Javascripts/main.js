d1711c5c1cfb236c510da7b090ba23e2decfead9
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-05 11:32:10 -0500

    clean up main js and modulize browserify links in main JS

--- Javascripts/main.js
+++ Javascripts/main.js
@@ -1,8 +1,10 @@
 "use strict";
 
-let domStuff = require('./domStuff');
-let faves = require('./favorites');
-let users = require('./users');
+let SongFaves = {
+	domStuff: require('./domStuff');
+	faves: require('./favorites');
+	users: require('./users');
+}
 
 
 $("select").change( function() {
@@ -10,13 +12,13 @@ $("select").change( function() {
   let selectedUser = $(this).val();
 //Without promises, we kick off the chain of events, but have to navigate from file to file to see what happens next
 //With Promises, all the execution of functions in the order we want/need is contained here in one place, making it easier to understand the sequence of events
-  SongFaves.UserFactory.getUsers(selectedUser)
+  SongFaves.users.getUsers(selectedUser)
   .then( (dataFromGetUsers) => { 
-  	return SongFaves.FavesFactory.getFaves(dataFromGetUsers);
+  	return SongFaves.faves.getFaves(dataFromGetUsers);
 	})
 	.then( (dataFromGetFaves) => {
 		// do something with the data
-		SongFaves.DomStuff.listFaves(selectedUser, dataFromGetFaves);
+		SongFaves.domStuff.listFaves(selectedUser, dataFromGetFaves);
 	})
 	.catch( (err) => {
 		console.log("Oops, there was an error:", err.statusText);
